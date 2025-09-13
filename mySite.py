9a322f444ee82d00aaed8c48177a9899c6dd6cfa
Yugal <yugaljagtap16@gmail.com>
2025-09-13 16:52:25 +0200

    add updated webpage

--- mySite.py
+++ mySite.py
@@ -1,6 +1,6 @@
 # import the necessary packages
 from flask import Flask, render_template, redirect, url_for, request,session,Response
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from supportFile import predict
 import os
 import cv2
@@ -27,18 +27,33 @@ def info():
 
 @app.route('/image', methods=['GET', 'POST'])
 def image():
-	if request.method == 'POST':
-		if request.form['sub']=='Upload':
-			savepath = r'upload/'
-			photo = request.files['photo']
-			photo.save(os.path.join(savepath,(secure_filename(photo.filename))))
-			image = cv2.imread(os.path.join(savepath,secure_filename(photo.filename)))
-			cv2.imwrite(os.path.join("static/images/","test_image.jpg"),image)
-			return render_template('image.html')
-		elif request.form['sub'] == 'Test':
-			result,pesticide,pesticide1,pesticide2= predict()
-			return render_template('image.html',result=result, pesticide=pesticide, pesticide1=pesticide1, pesticide2=pesticide2)
-	return render_template('image.html')
+    if request.method == 'POST':
+        savepath = r'upload/'
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+
+        # Check if the 'Test' button was clicked
+        if 'sub' in request.form and request.form['sub'] == 'Test':
+            result, pesticide, pesticide1, pesticide2 = predict()
+            return render_template('image.html', result=result, pesticide=pesticide, pesticide1=pesticide1, pesticide2=pesticide2)
+        
+        # Check if a file was uploaded (for the automatic submission)
+        elif 'photo' in request.files and request.files['photo'].filename != '':
+            photo = request.files['photo']
+            filename = secure_filename(photo.filename)
+            destination = os.path.join(savepath, filename)
+            photo.save(destination)
+            
+            image = cv2.imread(destination)
+            cv2.imwrite(os.path.join("static/images/", "test_image.jpg"), image)
+            # Add a success message for the user
+            return render_template('image.html', result="Image uploaded successfully! Click 'Test' to analyze.")
+
+        # Handle case where form is submitted without a file (e.g., empty submit)
+        else:
+            return render_template('image.html', result="Please select an image to upload.")
+
+    return render_template('image.html')
 
 # No caching at all for API endpoints.
 @app.after_request
--- supportFile.py
+++ supportFile.py
@@ -21,7 +21,7 @@ def predict():
     #resize the image to a 224x224 with the same strategy as in TM2:
     #resizing the image to be at least 224x224 and then cropping from the center
     size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
     #turn the image into a numpy array
     image_array = np.asarray(image)
